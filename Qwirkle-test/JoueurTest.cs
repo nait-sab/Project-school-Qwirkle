436ddc8e365e42003bf620558a4429e95733e1a8
Nait-sab <naitsab.boulogne@gmail.com>
2019-05-06 20:46:08 +0200

    Version 1.07

---
+++ Qwirkle-test/JoueurTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Qwirkle_lib;
+
+namespace Qwirkle_test
+{
+    [TestClass]
+    public class JoueurTest
+    {
+        [TestMethod]
+        public void TestConstructeur()
+        {
+            Joueur pierre = new Joueur("Pierre", 12, null);
+
+            Assert.AreEqual("Pierre", pierre.getNom());
+            Assert.AreEqual(12, pierre.getAge());
+            Assert.AreEqual(null, pierre.getMain());
+        }
+    }
+}
---
+++ Qwirkle/frm_qwirkle_menu.vb
@@ -0,0 +1,18 @@
+﻿Public Class frm_qwirkle_menu
+    Private Sub btn_nouvelle_partie_MouseHover(sender As Object, e As EventArgs) Handles btn_nouvelle_partie.MouseHover
+        btn_nouvelle_partie.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub btn_nouvelle_partie_MouseClick(sender As Object, e As MouseEventArgs) Handles btn_nouvelle_partie.MouseClick
+        frm_nouvelle_partie.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub frm_qwirkle_menu_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim validation As DialogResult
+        validation = MessageBox.Show("Quitter Qwirkle ?", "Qwirkle", MessageBoxButtons.YesNo)
+        If (validation = DialogResult.No) Then
+            e.Cancel = True
+        End If
+    End Sub
+End Class
