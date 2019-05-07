912193641e9e745065d78aac7292d546b65a389d
Blanchard cyril <cyril.blanchard.80@gmail.com>
2019-05-07 11:09:04 +0200

    delete erreur de commit

--- Qwirkle/frm_nouvelle_partie.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class frm_nouvelle_partie
-    Private Sub frm_nouvelle_partie_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        Dim validation As DialogResult
-        validation = MessageBox.Show("Quitter Qwirkle ?", "Qwirkle", MessageBoxButtons.YesNo)
-        If (validation = DialogResult.No) Then
-            e.Cancel = True
-        End If
-    End Sub
-End Class
